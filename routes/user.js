992d90e0889170bbc0d2fedf15864479c9792177
sunzhe <sunzhe@hefantv.com>
2018-09-03 12:00:39 +0800

    添加登录

--- routes/user.js
+++ routes/user.js
@@ -1,9 +1,9 @@
+const bcrypt = require('bcrypt');
 const userModule = require('../models/user');
-const bcrypt = require('bcrypt') ;
 
 module.exports = {
-  async signup(ctx, next) {
-    if(ctx.method == 'GET'){
+  async signup(ctx) {
+    if (ctx.method === 'GET') {
       await ctx.render('signup', {
         title: '注册',
       });
@@ -11,6 +11,7 @@ module.exports = {
     }
 
     const salt = await bcrypt.genSalt(10);
+
     let {
       name,
       email,
@@ -20,7 +21,8 @@ module.exports = {
 
     password = await bcrypt.hash(password, salt);
 
-    console.log('account = '+account);
+    console.log(`account = ${account}`);
+
     const user = {
       name,
       email,
@@ -28,16 +30,12 @@ module.exports = {
       account,
     };
 
-    console.log('user = '+ user);
+    console.log(`user = ${user}`);
 
-    console.log('开始数据库操作，date = '+new Date());
+    console.log(`开始数据库操作，date = ${new Date()}`);
     const result = await userModule.create(user);
 
-    console.log('数据库操作结束, result='+result+", date = "+ new Date());
+    console.log(`数据库操作结束, result=${result}, date = ${new Date()}`);
     ctx.body = result;
-
   },
 };
-
-
-
