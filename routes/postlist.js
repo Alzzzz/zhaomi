07fa53339fc6a1ce0fca19e027b09074dec26462
sunzhe <sunzhe@hefantv.com>
2018-09-05 18:12:24 +0800

    添加分页

--- routes/postlist.js
+++ routes/postlist.js
@@ -3,7 +3,15 @@ const CategoryModule = require('../models/category');
 
 module.exports = {
   async index(ctx) {
+    const pageSize = 15;
+    const currentPage = parseInt(ctx.query.page) || 1;
+    const allPostsCount = await PostModule.count();
+    const pageCount = Math.ceil(allPostsCount / pageSize);
+    const pageStart = currentPage - 2 > 0 ? currentPage - 2 : 1
+    const pageEnd = pageStart + 4 >= pageCount ? pageCount : pageStart + 4
+
     const cname = ctx.query.c;
+    const baseUrl =  cname ? `${ctx.path}?c=${cname}&page=` : `${ctx.path}?page=`
     console.log(cname);
     let cid;
     if (cname) {
@@ -11,11 +19,16 @@ module.exports = {
       cid = cateogry._id
     }
     const query = cid ? { category: cid } : {}
-    const posts = await PostModule.find(query)
+    const posts = await PostModule.find(query).skip((currentPage - 1)* pageSize).limit(pageSize);
     await ctx.render('index', {
       title: '着迷',
       desc: 'Z&Jane 高端私人定制',
       posts,
+      currentPage,
+      pageCount,
+      pageStart,
+      pageEnd,
+      baseUrl,
     });
   },
 };
